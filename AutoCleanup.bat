1866c415210718ae2d0503951a5b72ade4fc8605
nvhoang2803 <nvhg2803@gmail.com>
2023-10-04 10:53:56 +0700

    update expired day

--- AutoCleanup.bat
+++ AutoCleanup.bat
@@ -1,4 +1,4 @@
 @echo off
 set folderPath=C:\Users\Hoang\Downloads\autocleanup
-forfiles /p %folderPath% /s /m *.* /D -7 /C "cmd /c del @file"
+forfiles /p %folderPath% /s /m *.* /D -3 /C "cmd /c del @file"
 exit
