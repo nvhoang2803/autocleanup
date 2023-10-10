8936555948dcf6774712a7af1b476ad960329823
nvhoang2803 <nvhg2803@gmail.com>
2023-10-10 16:42:56 +0700

    update

--- AutoCleanup.bat
+++ AutoCleanup.bat
@@ -1,4 +1,8 @@
 @echo off
-set folderPath=C:\Users\Hoang\Downloads\autocleanup
-forfiles /p %folderPath% /s /m *.* /D -3 /C "cmd /c del @file"
+set downloadPath=C:\Users\Hoang\Downloads\autocleanup
+set screenshotPath=C:\Users\Hoang\Pictures\Screenshots>
+
+forfiles /p %downloadPath% /s /m *.* /D -3 /C "cmd /c del @file"
+forfiles /p %screenshotPath% /s /m *.* /D -3 /C "cmd /c del @file"
+
 exit
